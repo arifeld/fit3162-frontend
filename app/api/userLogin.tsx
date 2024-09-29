316ab9bdab754e6a0bbb4d5e584fc0baab03d0cc
Suryadeep Singh <ssin0061@student.monash.edu>
2024-09-29 17:33:07 +1000

    user loggin api

---
+++ app/api/userLogin.tsx
@@ -0,0 +1,24 @@
+/**
+ * API Requests for stores
+ */
+
+import axiosClient from "./requests"
+
+import Constants from "expo-constants";
+
+const DEV_API = "http://"+Constants.expoConfig?.hostUri?.split(':').shift()?.concat(':4000')
+const ROOT_URL = (process.env.NODE_ENV === "production" ? process.env.API_ENDPOINT : DEV_API)
+
+
+export const createUser = async ( userId: number, user_email: string, user_password: string, user_username: string) => {
+    const request = {
+        "user_id": userId,
+        "user_email": user_email,
+        "user_password": user_password ,
+        "user_username": user_username,
+    }
+
+    console.log(request)
+
+    return axiosClient.post("user", request);
+}
